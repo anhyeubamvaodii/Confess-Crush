7396a5881a9477a3a2c810bf02515c76db455e30
anhyeubamvaodii <106522401+anhyeubamvaodii@users.noreply.github.com>
2022-05-30 16:09:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Dành cho anh iu",
+    introTitle: 'Huỳnh Ngọc Long',
+    introDesc: `Em mới mò được cái này
+    Làm linh tinh cho anh iu xem nha :v
+    Iu anh
+    Love U`,
+    btnIntro: 'Hello gà iu',
+    title: 'Nó bắt em điền title nè hic',
+    desc: 'Yêu em honggg ? ',
+    btnYes: 'Ghét em lắmm',
+    btnNo: 'Yêu em lắm mà không bấm được chứ gì :3',
+    question: 'Ai cho ghét emmm <3',
+    btnReply: 'Trả lời đi',
+    reply: 'Khúc này bàn phím bị hư rồi chớ gì =))',
+    mess: 'Đáng đời tội ghét em',
+    messDesc: 'Nào gặp tét đít cái nha anh ơii.',
+    btnAccept: 'Love diuuu <3',
+    messLink: 'https://www.facebook.com/messages/t/100008560832596' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
